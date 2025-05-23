fd03135ba13fde4afb821f31d604f7b8dbf14f9f
Hayk-Darbinyan <haykdarbinyan04@gmail.com>
2025-05-23 12:13:05 +0400

    refactor: add public frontend url into cors

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://si-blue-frontend.onrender.com'],
   }),
 );
 configurePassport();
