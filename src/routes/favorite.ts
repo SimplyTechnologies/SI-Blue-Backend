bd911bd6d08c825f5d79ba517d4b032a49bea662
haykgyulabyan <gyulabyanh@gmail.com>
2025-05-29 12:08:49 +0400

    fix: get userId from auth middleware in favorite add/delete

--- src/routes/favorite.ts
+++ src/routes/favorite.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { createFavorite,  getFavoritesByUserId, deleteFavoriteById } from '../controllers/favorite';
+import { authenticateToken } from '../middlewares/vehicleDataValidator';
 
 const router = Router();
 
-router.post('/', createFavorite);
+router.post('/', authenticateToken, createFavorite);
 router.get('/:userId', getFavoritesByUserId);
-router.delete('/', deleteFavoriteById);
+router.delete('/:vehicleId', authenticateToken, deleteFavoriteById);
 
 export default router;
