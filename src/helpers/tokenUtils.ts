c99ea230e2a0e3c92aef5eba1053c9aa1ad12747
SatenikGabrielyan <gabrielyansatenik1@gmail.com>
2025-06-13 11:42:38 +0400

    change expiresIn

--- src/helpers/tokenUtils.ts
+++ src/helpers/tokenUtils.ts
@@ -6,14 +6,14 @@ const JWT_SECRET = process.env.JWT_SECRET as string;
 const REFRESH_SECRET = process.env.JWT_REFRESH_SECRET as string;
 
 export const generateAccessToken = (user: User, expiresIn?: StringValue) => {
-  return jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: expiresIn || '15m' });
+  return jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: expiresIn || '3m' });
 };
 
 export const generateRefreshToken = (user: User, remember: boolean) => {
   return jwt.sign({ id: user.id }, REFRESH_SECRET, { expiresIn: remember ? '30d' : '1d' });
 };
 export const generateTokenForAccountActivation = (user: User) => {
-  return jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn:  '1d' });
+  return jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn:  '3m' });
 };
 
 export const verifyRefreshToken = (token: string) => {
